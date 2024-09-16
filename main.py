e2c329add650ea412fac4001f28913c47897ee7e
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-16 09:45:37 +0500

    Create a gram matrix function

--- main.py
+++ main.py
@@ -33,3 +33,10 @@ x_img_style = keras.applications.vgg19.preprocess_input(np.expand_dims(img_style
 
 def get_content_loss(base_content, target):
   return tf.reduce_mean(tf.square(base_content - target))
+
+def gram_matrix(input_tensor):
+  channels = int(input_tensor.shape[-1])
+  a = tf.reshape(input_tensor, [-1, channels])
+  n = tf.shape(a)[0]
+  gram = tf.matmul(a, a, transpose_a=True)
+  return gram / tf.cast(n, tf.float32)
