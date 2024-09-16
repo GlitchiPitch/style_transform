04e053e0e09bf8367ca1e4d745fe71fe504eb1c3
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-16 09:47:04 +0500

    Create a compute loss function

--- main.py
+++ main.py
@@ -44,3 +44,27 @@ def gram_matrix(input_tensor):
 def get_style_loss(base_style, gram_target):
   gram_style = gram_matrix(base_style)
   return tf.reduce_mean(tf.square(gram_style - gram_target))
+
+def compute_loss(model, loss_weights, init_image, gram_style_features, content_features):
+  style_weight, content_weight = loss_weights
+  model_outputs = model(init_image)
+
+  style_outputs_features = model_outputs[:num_style_layers]
+  content_outputs_features = model_outputs[:num_content_layers]
+
+  style_score = 0
+  content_score = 0
+
+  weigth_per_style = 1.0 / float(num_style_layers)
+  for target_style, comb_style in zip(gram_style_features, style_outputs_features):
+    style_score += weigth_per_style * get_style_loss(comb_style[0], target_style)
+
+  weight_per_content_layer = 1.0 / float(num_content_layers)
+  for target_content, comb_content in zip(content_features, content_outputs_features):
+    content_score += weight_per_content_layer * get_content_loss(comb_content[0], target_content)
+
+  style_score *= style_weight
+  content_score *= content_weight
+
+  loss = style_score + content_score
+  return loss, style_score, content_score
