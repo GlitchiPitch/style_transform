b76d68d068afc172d94c811c3c213c1e959cd8a9
GlitchiPitch <nikitareutsky@gmail.com>
2024-09-16 09:33:11 +0500

    Create a model

---
+++ main.py
@@ -0,0 +1,25 @@
+from tensorflow import keras
+from PIL import Image
+import numpy as np
+
+content_layers = ['block5_conv2']
+style_layers = [
+    'block1_conv1',
+    'block2_conv1',
+    'block3_conv1',
+    'block4_conv1',
+    'block5_conv1'
+]
+
+num_content_layers = len(content_layers)
+num_style_layers = len(style_layers)
+
+vgg = keras.applications.vgg19.VGG19(include_top=False, weights="imagenet")
+vgg.trainable = False
+
+style_outputs = [vgg.get_layer(name).output for name in style_layers]
+content_outputs = [vgg.get_layer(name).output for name in content_layers]
+model_outputs = style_outputs + content_outputs
+
+model = keras.models.Model(vgg.input, model_outputs)
+print(model.summary())
